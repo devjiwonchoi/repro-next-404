499004cc2985a2d2ebb9b683645b7710e77d1c88
devjiwonchoi <devjiwonchoi@gmail.com>
2024-06-24 23:17:46 +0900

    init

---
+++ app/404/page.tsx
@@ -0,0 +1,4 @@
+/** Add your relevant code here for the issue to reproduce */
+export default function Home() {
+  return <p>check out console: status 404, it has noindex on head as well</p>
+}
---
+++ app/page.tsx
@@ -0,0 +1,4 @@
+/** Add your relevant code here for the issue to reproduce */
+export default function Home() {
+  return <a href="/404">go to /404</a>
+}
